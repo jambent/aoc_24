dc593cba72ecf7a17067b658095419b2615d28d5
jwbennett <jwbennett@Laptop>
2024-12-01 21:48:01 +0000

    Solution that produces total distance that's too large

--- day_001/sorted_lists_total_dist.c
+++ day_001/sorted_lists_total_dist.c
@@ -39,9 +39,61 @@ int main (int argc, char *argv[]) {
         array_population_index++;
     }
 
-    printf("Last array elements: %d %d\n", list_one_array[line_count-1], list_two_array[line_count-1]);
-
     fclose(fp);
+
+
+    //Selection sort algorithm
+    int *target_value, *lowest_value, temp;
+    // Sort list_one_array
+    for (int i = 0; i < line_count - 1; i++){
+        target_value = &list_one_array[i];
+        lowest_value = &list_one_array[i];
+        
+        for (int j = i + 1; j < line_count; j++){
+            if (list_one_array[j] < *lowest_value){
+                lowest_value = &list_one_array[j];
+            }
+        }
+        if (*lowest_value < *target_value){
+            temp = *target_value;
+            *target_value = *lowest_value;
+            *lowest_value = temp;
+        }
+    }
+
+    // Sort list_two_array
+    for (int i = 0; i < line_count - 1; i++){
+        target_value = &list_two_array[i];
+        lowest_value = &list_two_array[i];
+        
+        for (int j = i + 1; j < line_count; j++){
+            if (list_two_array[j] < *lowest_value){
+                lowest_value = &list_two_array[j];
+            }
+        }
+        if (*lowest_value < *target_value){
+            temp = *target_value;
+            *target_value = *lowest_value;
+            *lowest_value = temp;
+        }
+    }
+
+    // Calculate total distance between sorted lists
+    int total_distance = 0;
+
+    for (int i = 0; i < line_count - 1; i++){
+        total_distance += abs(list_one_array[i] - list_two_array[i]);
+    }
     
+    printf("Total distance between sorted lists: %d\n", total_distance);
+    // printf("List one: \n");
+    // for (int i = 0; i < line_count - 1; i++){
+    //     printf("%d\n", list_one_array[i]);
+    // }
+    // printf("List two: \n");
+    // for (int i = 0; i < line_count - 1; i++){
+    //     printf("%d\n", list_two_array[i]);
+    // }
+        
     return 0;
 }
