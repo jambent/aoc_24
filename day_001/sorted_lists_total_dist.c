2f85075d36c1b5033933ec65993219d2b2ee9e7d
jwbennett <jwbennett@Laptop>
2024-12-01 20:24:16 +0000

    Reads first two lines of two integers into two integers successfully

--- day_001/sorted_lists_total_dist.c
+++ day_001/sorted_lists_total_dist.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FILENAME "./day_001/day_1_input.txt"
-
-
-int main (void){
+// Read test input text file using command line argument,
+// e.g., ./day_001/sorted_lists_total_dist ./day_001/day_1_input.txt
+int main (int argc, char *argv[]) {
 
     FILE *fp;
+    int list_one_integer, list_two_integer;
 
-    fp = fopen(FILENAME, "r");
+    fp = fopen(argv[1], "r");
 
-    if (fp == NULL) {
-        printf("Cannot open file %s\n", FILENAME);
+    if (argc != 2) {
+        printf("%s not called with single filename\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    printf("Test input can be opened successfully\n");
+    if (fp == NULL) {
+        printf("Cannot open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    fscanf(fp, "%d %d", &list_one_integer, &list_two_integer);
+    
+
+    printf("First integer in file found: %d\n", list_one_integer);
+    printf("Second integer in file found: %d\n", list_two_integer);
+
+    fscanf(fp, "%d %d", &list_one_integer, &list_two_integer);
+    
+
+    printf("First integer in file found: %d\n", list_one_integer);
+    printf("Second integer in file found: %d\n", list_two_integer);
 
     fclose(fp);
+    
 
     return 0;
 }
