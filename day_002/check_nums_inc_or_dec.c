0ebfd4ddb8f99cf551f133c254e72816d0d15af2
jwbennett <jwbennett@Laptop>
2024-12-02 21:32:42 +0000

    Produces result that is too high

--- day_002/check_nums_inc_or_dec.c
+++ day_002/check_nums_inc_or_dec.c
@@ -6,11 +6,10 @@
 int main (int argc, char *argv[]) {
 
     FILE *fp;
-    //char end_of_file_check;
     char *line;
-    line = malloc(100);
-    long int *line_number_array;
-    line_number_array = malloc(100);
+    line = calloc(100,sizeof(char));
+
+    
     int good_report_count = 0;
 
 
@@ -27,33 +26,36 @@ int main (int argc, char *argv[]) {
     }
 
 
-  //  while ((end_of_file_check = getc(fp))!= EOF) {
-        fgets(line, 100, fp);
-  
+    while ((fgets(line, 100, fp))!= NULL) {
+        
+        long int *line_number_array;
+        line_number_array = calloc(100,sizeof(long int));
 
+        line[strcspn(line, "\n")] = '\0';
+  
         char *endptr;
         long int num;
 
-        
+        int token_count = 1;
         char *token = strtok(line, " ");
+        if (token != NULL){
         num = strtol(token, &endptr, 10);
         line_number_array[0] = num;
-        printf("%ld\n", num);
-        int token_count = 1;
+        
         
             while (token!=NULL) {
                 
                 token = strtok(NULL, " ");
                 if (token != NULL) {
-                num = strtol(token, &endptr, 10);
-                printf("%ld\n", num);
-                line_number_array[token_count] = num;
-                token_count++;
-            }
+                    num = strtol(token, &endptr, 10);
+                    line_number_array[token_count] = num;
+                    token_count++;
+                }
             }
 
-        printf("Token count is %d\n", token_count);
-    
+        }
+       
+
 
         int report_good = 1;
         if ((line_number_array[1] - line_number_array[0]) > 0){
@@ -61,6 +63,7 @@ int main (int argc, char *argv[]) {
             for (int i = 2; i < token_count; i++) {
                 if (((line_number_array[i] - line_number_array[i-1]) < 0) || ((line_number_array[i] - line_number_array[i-1]) == 0) || ((line_number_array[i] - line_number_array[i-1]) > 3)) {
                     report_good = 0;
+                    break;
                 }
             }
         
@@ -70,16 +73,25 @@ int main (int argc, char *argv[]) {
                 if (((line_number_array[i] - line_number_array[i-1]) > 0) || ((line_number_array[i] - line_number_array[i-1]) == 0) || (abs(line_number_array[i] - line_number_array[i-1]) > 3))
                 {
                     report_good = 0;
+                    break;
                 }
             }
+        }
+        if ((line_number_array[1] - line_number_array[0]) == 0){
+            report_good = 0;
         }
 
         good_report_count += report_good;
 
-        free(line);
         free(line_number_array);
-        fclose(fp);
         
-        printf("Number of good reports: %d\n", good_report_count);
-        return 0;
+
+    }
+
+    free(line);
+   
+    fclose(fp);
+    
+    printf("Number of good reports: %d\n", good_report_count);
+    return 0;
 }
