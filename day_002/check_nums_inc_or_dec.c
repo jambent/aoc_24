1f565510db97f16d328e9668cd0cbdc60ea21317
jwbennett <jwbennett@Laptop>
2024-12-02 22:06:38 +0000

    Working solution

--- day_002/check_nums_inc_or_dec.c
+++ day_002/check_nums_inc_or_dec.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 
 int main (int argc, char *argv[]) {
 
     FILE *fp;
     char *line;
-    line = calloc(100,sizeof(char));
+    line = calloc(256,sizeof(char));
 
     
     int good_report_count = 0;
@@ -26,58 +27,51 @@ int main (int argc, char *argv[]) {
     }
 
 
-    while ((fgets(line, 100, fp))!= NULL) {
+    while ((fgets(line, 256, fp))!= NULL) {
         
         long int *line_number_array;
-        line_number_array = calloc(100,sizeof(long int));
+        line_number_array = calloc(256,sizeof(long int));
 
         line[strcspn(line, "\n")] = '\0';
   
         char *endptr;
         long int num;
-
-        int token_count = 1;
+        int token_count = 0;
         char *token = strtok(line, " ");
-        if (token != NULL){
-        num = strtol(token, &endptr, 10);
-        line_number_array[0] = num;
-        
-        
-            while (token!=NULL) {
-                
-                token = strtok(NULL, " ");
-                if (token != NULL) {
-                    num = strtol(token, &endptr, 10);
-                    line_number_array[token_count] = num;
-                    token_count++;
-                }
-            }
 
+
+        while (token != NULL) {
+            num = strtol(token, &endptr, 10);
+            line_number_array[token_count] = num;
+            token_count++;
+            token = strtok(NULL, " ");
         }
        
-
-
         int report_good = 1;
-        if ((line_number_array[1] - line_number_array[0]) > 0){
+        
+        if (token_count < 2) {
+            report_good = 0;
+        }
+        else if ((line_number_array[1] - line_number_array[0]) > 0){
             
-            for (int i = 2; i < token_count; i++) {
-                if (((line_number_array[i] - line_number_array[i-1]) < 0) || ((line_number_array[i] - line_number_array[i-1]) == 0) || ((line_number_array[i] - line_number_array[i-1]) > 3)) {
+            for (int i = 1; i < token_count; i++) {
+                if (((line_number_array[i] - line_number_array[i-1]) <= 0) || ((line_number_array[i] - line_number_array[i-1]) > 3)) {
                     report_good = 0;
                     break;
                 }
             }
         
         }
-        if ((line_number_array[1] - line_number_array[0]) < 0){
-            for (int i = 2; i < token_count; i++) {
-                if (((line_number_array[i] - line_number_array[i-1]) > 0) || ((line_number_array[i] - line_number_array[i-1]) == 0) || (abs(line_number_array[i] - line_number_array[i-1]) > 3))
+        else if ((line_number_array[1] - line_number_array[0]) < 0){
+            for (int i = 1; i < token_count; i++) {
+                if (((line_number_array[i] - line_number_array[i-1]) >= 0) || (abs(line_number_array[i] - line_number_array[i-1]) > 3))
                 {
                     report_good = 0;
                     break;
                 }
             }
         }
-        if ((line_number_array[1] - line_number_array[0]) == 0){
+        else {
             report_good = 0;
         }
 
